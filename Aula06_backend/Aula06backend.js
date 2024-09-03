343ac54334ea2722c17d363c291c1d94e2ba952e
Pairon03 <142590063+Pairon03@users.noreply.github.com>
2024-09-02 22:33:53 -0300

    Add files via upload

---
+++ Aula06_backend/Aula06backend.js
@@ -0,0 +1,121 @@
+ // Atividade 1
+ console.log("Atividade 1");
+   
+ const comidas = ['arroz', 'feijão', 'batata', 'macarrão', 'cebola'];
+
+ console.log(comidas[0]);
+
+ comidas[0] = 'cenoura';
+ console.log(comidas[0]);   
+
+ console.log(" ");
+
+// Atividade 2
+ console.log("Atividade 2");
+
+ const numeros = [];
+
+ numeros.push(4,2,3);
+ console.log(numeros);
+
+ numeros[0] = 8;
+ console.log(numeros);
+
+ console.log(" ");
+
+// Atividade 3
+ console.log("Atividade 3");
+
+ const num = [];
+
+ num.push(3,2,1);
+ console.log(num);
+
+ console.log(" ");
+
+// Atividade 4
+ console.log("Atividade 4");  
+
+const clinica = []
+
+clinica.push('macaco','cachorro','passaro');
+console.log(clinica);
+
+clinica.pop();
+console.log(clinica);
+
+clinica.pop();
+console.log(clinica);
+
+clinica.pop();
+console.log(clinica);
+
+ console.log(" ");
+
+// Atividade 5
+ console.log("Atividade 5");
+
+
+ console.log(" ");
+
+
+// Atividade 6
+ console.log("Atividade 6");
+
+ console.log(" ");
+
+// Atividade 7
+ console.log("Atividade 7");
+
+ console.log(" ");
+
+// Atividade 8
+ console.log("Atividade 8");
+
+
+ console.log(" ");
+
+// Atividade 9
+ console.log("Atividade 9");
+
+
+
+ console.log(" ");
+
+// Atividade 10
+ console.log("Atividade 10");
+
+
+ console.log(" ");
+
+// Atividade 11
+  console.log("Atividade 11");
+
+ console.log(" ");
+
+// Atividade 12
+  console.log("Atividade 12");
+
+
+  console.log(" ");
+
+// Atividade 13
+ console.log("Atividade 13");
+
+
+  
+  console.log(" ");
+
+// Atividade 14
+ console.log("Atividade 14");
+
+
+  
+ console.log(" ");
+
+// Atividade 15
+ console.log("Atividade 15");
+
+
+  
+ console.log(" ");
